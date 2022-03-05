a6f17486dab039eb4242adfe50a6e28260f24a37
Jaap <33700526+GewoonJaap@users.noreply.github.com>
2022-03-05 15:53:29 +0100

    Retry downloads from telegram

--- MessagePost/postMessage.js
+++ MessagePost/postMessage.js
@@ -4,6 +4,7 @@ const twitterPostModel = require('../Models/twitterPostModel');
 const telegramPostModel = require('../Models/telegramPostModel');
 const reutersMapModel = require('../Models/reutersMapModel');
 const fileUpload = require('../Helpers/fileUploader');
+const { UPLOAD_TRIES } = require('../consts.json');
 const fs = require('fs');
 
 async function postReutersMap(message, forceSend = false) {
@@ -48,8 +49,12 @@ async function postTelegramMessage(message, forceSend = false) {
 async function addMediaToTelegramMessage(messageObject) {
   try {
     if (messageObject.picture) {
+      let tries = 0;
       const fileName = `${messageObject.messageId.split('/')[0]}-${messageObject.messageId.split('/')[1]}-image.jpg`;
-      const result = await fileUpload.downloadAndUpload(messageObject.picture, fileName, 'picture');
+      let result = await fileUpload.downloadAndUpload(messageObject.picture, fileName, 'picture');
+      while (!result && ++tries < UPLOAD_TRIES) {
+        result = await fileUpload.downloadAndUpload(messageObject.picture, fileName, 'picture');
+      }
       if (result) {
         messageObject.picture = result.Location;
       }
@@ -61,8 +66,11 @@ async function addMediaToTelegramMessage(messageObject) {
   try {
     if (messageObject.video) {
       const fileName = `${messageObject.messageId.split('/')[0]}-${messageObject.messageId.split('/')[1]}-video.mp4`;
-
-      const result = await fileUpload.downloadAndUpload(messageObject.video, fileName, 'video');
+      let tries = 0;
+      let result = await fileUpload.downloadAndUpload(messageObject.video, fileName, 'video');
+      while (!result && ++tries < UPLOAD_TRIES) {
+        result = await fileUpload.downloadAndUpload(messageObject.video, fileName, 'video');
+      }
       if (result) {
         messageObject.video = result.Location;
       }
